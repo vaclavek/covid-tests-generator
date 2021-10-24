e5d78e47e962385fad87b98e9022f4427675ebd1
Ondřej Václavek <vaclavek@havit.cz>
2021-10-24 23:17:39 +0200

    Remove unused parts

--- Web.Client/Routes.cs
+++ Web.Client/Routes.cs
@@ -4,20 +4,5 @@
 	{
 		public const string Home = "/";
 		public const string New = "/new";
-
-		public static class Administration
-		{
-			public const string Index = "/admin/";
-		}
-
-		public static class UserAdministration
-		{
-			public const string PageName = "/admin/user/page-name";
-		}
-
-		public static class Diagnostics
-		{
-			public const string Info = "/diag/info";
-		}
 	}
 }
