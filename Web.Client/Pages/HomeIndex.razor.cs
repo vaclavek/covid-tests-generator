0f345073d7cfb0020199c10450c9e8c96fdf39d8
Svoboda, Petr <svoboda@havit.cz>
2021-10-27 17:31:53 +0200

    Localization Czech/English

---
+++ Web.Client/Pages/HomeIndex.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using CTG.CovidTestsGenerator.Web.Client.Resources.Pages;
+
+namespace CTG.CovidTestsGenerator.Web.Client.Pages
+{
+	public partial class HomeIndex
+	{
+		[Inject] protected IHomeIndexLocalizer HomeIndexLocalizer { get; set; }
+	}
+}
--- Web.Client/Pages/New.razor.cs
+++ Web.Client/Pages/New.razor.cs
@@ -5,6 +5,7 @@ using CTG.CovidTestsGenerator.Contracts;
 using CTG.CovidTestsGenerator.Web.Client.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using CTG.CovidTestsGenerator.Web.Client.Resources.Pages;
 
 namespace CTG.CovidTestsGenerator.Web.Client.Pages
 {
@@ -12,14 +13,19 @@ namespace CTG.CovidTestsGenerator.Web.Client.Pages
 	{
 		private readonly UserData model = new();
 
-		private readonly List<TestType> testTypes = new()
-		{
-			new TestType { Value = false, Title = "Negativní" },
-			new TestType { Value = true, Title = "Pozitivní" }
-		};
+		private readonly List<TestType> testTypes = new();
 
 		[Inject] public IJSRuntime JsRuntime { get; set; }
 		[Inject] public IPdfGenerator PdfGenerator { get; set; }
+		[Inject] protected INewLocalizer NewLocalizer { get; set; }
+
+		protected override void OnParametersSet()
+		{
+			base.OnParametersSet();
+
+			testTypes.Add(new TestType { Value = false, Title = NewLocalizer.Negative });
+			testTypes.Add(new TestType { Value = true, Title = NewLocalizer.Positive });
+		}
 
 		protected async Task GenerateAsync()
 		{
@@ -27,11 +33,11 @@ namespace CTG.CovidTestsGenerator.Web.Client.Pages
 
 			await JsRuntime.InvokeVoidAsync("jsSaveAsFile", $"Test-{model.TestDateTime.Value:yyyyMMdd}.pdf", Convert.ToBase64String(data));
 		}
-	}
 
-	internal class TestType
-	{
-		public bool Value { get; set; }
-		public string Title { get; set; }
+		internal class TestType
+		{
+			public bool Value { get; set; }
+			public string Title { get; set; }
+		}
 	}
 }
