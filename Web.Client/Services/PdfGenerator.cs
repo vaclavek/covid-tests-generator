d80d9ab74dbad9f131d0f6c85389b88e511e38b1
Ondřej Václavek <vaclavek@havit.cz>
2021-11-01 18:53:03 +0100

    Fixes not-disposed fonts GH-3

--- Web.Client/Services/PdfGenerator.cs
+++ Web.Client/Services/PdfGenerator.cs
@@ -16,21 +16,15 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 	public class PdfGenerator : IPdfGenerator
 	{
 		private readonly HttpClient httpClient;
-		private readonly PdfFont font;
 
 		public PdfGenerator(HttpClient httpClient)
 		{
 			this.httpClient = httpClient;
-			font = PdfFontFactory.CreateRegisteredFont("helvetica", "windows-1250");
 		}
 
 		public async Task<byte[]> GetPdfAsync(UserData userData)
 		{
-			// Marge in centimeter, then I convert with .ToDpi()
-			//float margeLeft = 1.5f;
-			//float margeRight = 1.5f;
-			//float margeTop = 1.0f;
-			//float margeBottom = 1.0f;
+			PdfFont font = PdfFontFactory.CreateRegisteredFont("helvetica", "windows-1250");
 
 			using var streamWithEmptyPdf = await httpClient.GetStreamAsync("/empty.pdf");
 			using var reader = new PdfReader(streamWithEmptyPdf);
@@ -38,6 +32,7 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 			using var writer = new PdfWriter(stream);
 			using var pdf = new PdfDocument(reader, writer);
 			using var document = new Document(pdf);
+			document.SetFont(font);
 
 			AddFullName(document, userData.FullName);
 			AddPernamentAddress(document, userData.PernamentAddress);
@@ -58,7 +53,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddFullName(Document document, string fullName)
 		{
 			var paragraph = new Paragraph(fullName);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 685, 1000);
 			document.Add(paragraph);
 		}
@@ -66,7 +60,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddPernamentAddress(Document document, string address)
 		{
 			var paragraph = new Paragraph(address);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 638, 1000);
 			document.Add(paragraph);
 
@@ -75,7 +68,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddCurrentAddress(Document document, string address)
 		{
 			var paragraph = new Paragraph(address);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 592, 1000);
 			document.Add(paragraph);
 		}
@@ -83,7 +75,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddDateOfBirth(Document document, DateTime dateOfBirth)
 		{
 			var paragraph = new Paragraph(dateOfBirth.ToString("dd. MM. yyyy"));
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 542, 1000);
 			document.Add(paragraph);
 		}
@@ -91,7 +82,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddPhoneNumber(Document document, string phoneNumber)
 		{
 			var paragraph = new Paragraph(phoneNumber);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(308, 542, 1000);
 			document.Add(paragraph);
 		}
@@ -99,7 +89,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddEmail(Document document, string email)
 		{
 			var paragraph = new Paragraph(email);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 506, 1000);
 			document.Add(paragraph);
 		}
@@ -107,7 +96,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddPassportOrIdNumber(Document document, string passportOrIdNumber)
 		{
 			var paragraph = new Paragraph(passportOrIdNumber);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(308, 506, 1000);
 			document.Add(paragraph);
 		}
@@ -119,13 +107,11 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 				return;
 			}
 			var paragraph = new Paragraph(testDateTime.Value.ToString("dd. MM. yyyy HH:mm:ss"));
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 360, 1000);
 			document.Add(paragraph);
 
 
 			var paragraphDate = new Paragraph(testDateTime.Value.ToString("dd. MM. yyyy"));
-			paragraphDate.SetFont(font);
 			paragraphDate.SetFixedPosition(100, 165, 1000);
 			document.Add(paragraphDate);
 		}
@@ -154,7 +140,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		{
 			string testName = "VivaDiag Pro SARS CoV 2 Ag Rapid test, VivaChek Biotech (Hangzhou) Co., Ltd.";
 			var paragraph = new Paragraph(testName);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 400, 1000);
 			document.Add(paragraph);
 		}
@@ -162,7 +147,6 @@ namespace CTG.CovidTestsGenerator.Web.Client.Services
 		private void AddTestPlace(Document document, string testPlace)
 		{
 			var paragraph = new Paragraph(testPlace);
-			paragraph.SetFont(font);
 			paragraph.SetFixedPosition(100, 317, 1000);
 			document.Add(paragraph);
 		}
