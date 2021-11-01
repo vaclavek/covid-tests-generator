21c7e490ed715e0eb82a977661644b2beadb3519
Ondřej Václavek <vaclavek@havit.cz>
2021-11-01 20:01:19 +0100

    Saved inserted values to local storage GH-4

--- Web.Client/Pages/New.razor.cs
+++ Web.Client/Pages/New.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using CTG.CovidTestsGenerator.Contracts;
 using CTG.CovidTestsGenerator.Web.Client.Resources.Pages;
 using CTG.CovidTestsGenerator.Web.Client.Services;
@@ -11,16 +12,23 @@ namespace CTG.CovidTestsGenerator.Web.Client.Pages
 {
 	public partial class New
 	{
-		private readonly UserData model = new();
+		private UserData model = new();
 
 		private readonly List<TestType> testTypes = new();
 
 		[Inject] public IJSRuntime JsRuntime { get; set; }
 		[Inject] public IPdfGenerator PdfGenerator { get; set; }
 		[Inject] protected INewLocalizer NewLocalizer { get; set; }
+		[Inject] private ILocalStorageService LocalStorage { get; set; }
 
-		protected override void OnInitialized()
+		private const string LocalStorageModelKey = "New_UserData";
+
+
+		protected override async Task OnInitializedAsync()
 		{
+			testTypes.Add(new TestType { Value = false, Title = NewLocalizer.Negative });
+			testTypes.Add(new TestType { Value = true, Title = NewLocalizer.Positive });
+
 #if DEBUG
 			model.FullName = "Jan Novák";
 			model.PernamentAddress = "Jiráskova 7, 739 11 Frýdlant nad Ostravicí";
@@ -33,18 +41,20 @@ namespace CTG.CovidTestsGenerator.Web.Client.Pages
 			model.TestType = false;
 			model.TestPlace = "Medical Testing s.r.o., AG CovidPoint, Želetavská ul., 140 00 Praha";
 #endif
-		}
 
-		protected override void OnParametersSet()
-		{
-			base.OnParametersSet();
-
-			testTypes.Add(new TestType { Value = false, Title = NewLocalizer.Negative });
-			testTypes.Add(new TestType { Value = true, Title = NewLocalizer.Positive });
+			// load from local storage, if some model has been already saved
+			var savedModel = await LocalStorage.GetItemAsync<UserData>(LocalStorageModelKey);
+			if (savedModel != null)
+			{
+				model = savedModel;
+			}
 		}
 
 		protected async Task GenerateAsync()
 		{
+			// save to local storage
+			await LocalStorage.SetItemAsync(LocalStorageModelKey, model);
+
 			var data = await PdfGenerator.GetPdfAsync(model);
 
 			await JsRuntime.InvokeVoidAsync("jsSaveAsFile", $"Test-{model.TestDateTime.Value:yyyyMMdd}.pdf", Convert.ToBase64String(data));
