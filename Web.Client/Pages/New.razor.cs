5d4ee8819fc3c513bf1bd9f1ca32d58a1a1b6da0
Ondřej Václavek <vaclavek@havit.cz>
2021-11-01 19:11:36 +0100

    Change test data

--- Web.Client/Pages/New.razor.cs
+++ Web.Client/Pages/New.razor.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CTG.CovidTestsGenerator.Contracts;
+using CTG.CovidTestsGenerator.Web.Client.Resources.Pages;
 using CTG.CovidTestsGenerator.Web.Client.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using CTG.CovidTestsGenerator.Web.Client.Resources.Pages;
 
 namespace CTG.CovidTestsGenerator.Web.Client.Pages
 {
@@ -18,6 +18,22 @@ namespace CTG.CovidTestsGenerator.Web.Client.Pages
 		[Inject] public IJSRuntime JsRuntime { get; set; }
 		[Inject] public IPdfGenerator PdfGenerator { get; set; }
 		[Inject] protected INewLocalizer NewLocalizer { get; set; }
+
+		protected override void OnInitialized()
+		{
+#if DEBUG
+			model.FullName = "Jan Novák";
+			model.PernamentAddress = "Jiráskova 7, 739 11 Frýdlant nad Ostravicí";
+			model.CurrentAddress = "Opletalova 1603/57, 110 00 Nové Město";
+			model.DateOfBirth = new DateTime(1995, 10, 31);
+			model.PhoneNumber = "+420 736 111 233";
+			model.Email = "jan.novak@example.cz";
+			model.PassportOrIdNumber = "123 457 887";
+			model.TestDateTime = DateTime.Now;
+			model.TestType = false;
+			model.TestPlace = "Medical Testing s.r.o., AG CovidPoint, Želetavská ul., 140 00 Praha";
+#endif
+		}
 
 		protected override void OnParametersSet()
 		{
