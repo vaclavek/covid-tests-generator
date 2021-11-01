f35ba7ff73c52d0360a15d419fdbceccec79766c
Ondřej Václavek <vaclavek@havit.cz>
2021-11-01 19:11:36 +0100

    Change test data

--- Web.Client/Pages/New.razor.cs
+++ Web.Client/Pages/New.razor.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CTG.CovidTestsGenerator.Contracts.Model;
+using CTG.CovidTestsGenerator.Contracts;
+using CTG.CovidTestsGenerator.Web.Client.Resources.Pages;
 using CTG.CovidTestsGenerator.Web.Client.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using CTG.CovidTestsGenerator.Web.Client.Resources.Pages;
 
 namespace CTG.CovidTestsGenerator.Web.Client.Pages
 {
@@ -14,54 +14,45 @@ namespace CTG.CovidTestsGenerator.Web.Client.Pages
 		private readonly UserData model = new();
 
 		private readonly List<TestType> testTypes = new();
-		private readonly List<TimeType> hourTypes = new();
-		private readonly List<TimeType> MinuteTypes = new();
 
 		[Inject] public IJSRuntime JsRuntime { get; set; }
 		[Inject] public IPdfGenerator PdfGenerator { get; set; }
 		[Inject] protected INewLocalizer NewLocalizer { get; set; }
-		[Inject] protected CTG.CovidTestsGenerator.Resources.Model.IUserDataLocalizer UserDataLocalizer { get; set; }
 
 		protected override void OnInitialized()
 		{
-			base.OnInitialized();
+#if DEBUG
+			model.FullName = "Jan Novák";
+			model.PernamentAddress = "Jiráskova 7, 739 11 Frýdlant nad Ostravicí";
+			model.CurrentAddress = "Opletalova 1603/57, 110 00 Nové Město";
+			model.DateOfBirth = new DateTime(1995, 10, 31);
+			model.PhoneNumber = "+420 736 111 233";
+			model.Email = "jan.novak@example.cz";
+			model.PassportOrIdNumber = "123 457 887";
+			model.TestDateTime = DateTime.Now;
+			model.TestType = false;
+			model.TestPlace = "Medical Testing s.r.o., AG CovidPoint, Želetavská ul., 140 00 Praha";
+#endif
+		}
+
+		protected override void OnParametersSet()
+		{
+			base.OnParametersSet();
 
 			testTypes.Add(new TestType { Value = false, Title = NewLocalizer.Negative });
 			testTypes.Add(new TestType { Value = true, Title = NewLocalizer.Positive });
-
-			for (int hour = 0; hour <= 23; hour++)
-			{
-				hourTypes.Add(new TimeType { Value = hour, Title = hour.ToString("D2") });
-			}
-			for (int minute = 0; minute <= 59; minute += 5)
-			{
-				MinuteTypes.Add(new TimeType { Value = minute, Title = minute.ToString("D2") });
-			}
-
-			model.FullName = UserDataLocalizer.DefaultFullName;
-			model.PermanentAddress = UserDataLocalizer.DefaultPermanentAddress;
-			model.CurrentAddress = UserDataLocalizer.DefaultCurrentAddress;
-			model.Email = UserDataLocalizer.DefaultEmail;
-			model.TestPlace = UserDataLocalizer.DefaultTestPlace;
-			model.PhoneNumber = UserDataLocalizer.DefaultPhoneNumber;
 		}
 
 		protected async Task GenerateAsync()
 		{
 			var data = await PdfGenerator.GetPdfAsync(model);
 
-			await JsRuntime.InvokeVoidAsync("jsSaveAsFile", $"Test-{model.TestDate:yyyyMMdd}.pdf", Convert.ToBase64String(data));
+			await JsRuntime.InvokeVoidAsync("jsSaveAsFile", $"Test-{model.TestDateTime.Value:yyyyMMdd}.pdf", Convert.ToBase64String(data));
 		}
 
 		internal class TestType
 		{
 			public bool Value { get; set; }
-			public string Title { get; set; }
-		}
-
-		internal class TimeType
-		{
-			public int Value { get; set; }
 			public string Title { get; set; }
 		}
 	}
